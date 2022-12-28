790f0744e49c04558f6f76975f11caac6a6871d2
softmanager266 <121472552+softmanager266@users.noreply.github.com>
2022-12-28 07:27:28 -0300

    Update TablaCongreso.java
    Aumento de string tabla image a 900

--- src/main/java/com/supraweb/roles/controller/congreso/TablaCongreso.java
+++ src/main/java/com/supraweb/roles/controller/congreso/TablaCongreso.java
@@ -201,7 +201,7 @@ public class TablaCongreso  {
 	public void setNombreDelProducto(String nombreDelProducto) {
 		this.nombreDelProducto = nombreDelProducto;
 	}
-	@Column(name = "imagenes", nullable = false, length = 100)
+	@Column(name = "imagenes", nullable = false, length = 800)
 	public String getImagenes() {
 		return this.imagenes;
 	}
